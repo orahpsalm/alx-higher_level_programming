1595f57a815f43d07dcc19d3e6f27801e57bd782
orahpsalm <psalmorahcle@gmail.com>
2022-05-31 21:20:44 +0100

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number > 0:
     last_digit = number % 10
 elif number < 0:
-    last_digit = -number % 10
+    last_digit = -(-number % 10)
 elif number == 0:
     last_digit = 0
 
