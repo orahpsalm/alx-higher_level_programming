4186d515fadd82197b76a1dc975cb800a51e9efe
orahpsalm <psalmorahcle@gmail.com>
2022-06-02 17:14:12 +0100

    Modify 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
         print("{} arguements.".format(len(argv) - 1))
     elif len(argv) == 2:
         print("{} arguement:".format(len(argv) - 1))
-        print("{}: {}".format(len(argv) - 1, argv[1]))
-    elif len(argv) > 2:
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+    else:
         print("{} arguements:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
