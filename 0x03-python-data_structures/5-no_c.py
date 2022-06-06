688aad4a80f848096657bd65391516de561593a9
orahpsalm <psalmorahcle@gmail.com>
2022-06-06 14:06:07 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_list = list(my_string)
     new_string = ""
     for i in range(len(new_list)):
-        if new_list[i]!= "c" and new_list[i] != "C":
+        if new_list[i] != "c" and new_list[i] != "C":
             new_string += new_list[i]
     return new_string 
