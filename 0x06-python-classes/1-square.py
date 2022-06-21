c5d1ff7cc224214191d483a0408cbf67f4bd62c0
orahpsalm <psalmorahcle@gmail.com>
2022-06-21 20:05:19 +0100

    pycodestyle 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,7 @@
 class Square:
     def __init__(self, size):
         """ Initialize method.
-        
+
         Args:
             self (class): This class
             size (int): Size of the square
