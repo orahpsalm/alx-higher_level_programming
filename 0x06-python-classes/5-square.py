22894aabf1bd7383550856cfd3867c7b267201b5
orahpsalm <psalmorahcle@gmail.com>
2022-06-21 20:36:42 +0100

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""Define an object name Square.
+"""
+
+class Square:
+    """ Object Square [class]
+    """
+    def __init__(self, size=0):
+        """ Initialize method.
+        Args:
+            self (class): This class
+            size (int): Size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter  method that sets the size of square.
+        Args:
+            value (int): size of Square
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
+    
+    def my_print(self):
+        """Prints in stdout the square with the character #:"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
