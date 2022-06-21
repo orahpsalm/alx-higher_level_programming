09efc832c68351e856acfe2adcea6476a8d944d6
orahpsalm <psalmorahcle@gmail.com>
2022-06-21 19:56:36 +0100

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define an object, Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """ Initialize method.
+        
+        Args:
+            self (class): This class
+            size (int): Size of the square
+        """
+
+        """ Instance attribute
+        """
+        self.__size = size
