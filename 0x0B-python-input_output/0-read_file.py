5348b8f3e12163d666d00bd913993154af576f08
orahpsalm <psalmorahcle@gmail.com>
2022-07-05 19:47:39 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    """Print contents of a UTF8 text file to standard output."""
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
