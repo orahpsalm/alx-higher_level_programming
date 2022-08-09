71a91e7e45c97b8d79bf073b78fcf6b189dbbb61
orahpsalm <psalmorahcle@gmail.com>
2022-08-09 20:41:02 +0100

    Create 11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Show the score and name colums order numerically if the score >= 10
+-- Query that shows the score and name colums order numerically if the score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
