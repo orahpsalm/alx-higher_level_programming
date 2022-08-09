92c8904a0c2b396a0235dd04d96289f6dc9e3c5f
orahpsalm <psalmorahcle@gmail.com>
2022-08-09 21:19:37 +0100

    Update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Create database with name htbn_0c_0
 -- Query :creates hbn-0c_0 database in server if it is missing
-CREATE DATABASE IF NOT EXISTS htbn_0c_0;
+CREATE DATABASE IF NOT EXISTS `htbn_0c_0`;
