fc41e9a67a425404fc3b392b4a4cec480c4f2076
orahpsalm <psalmorahcle@gmail.com>
2022-08-09 20:45:42 +0100

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
--- Show the score and name colums order numerically if the score >= 10
--- Query that shows the score and name colums order numerically if the score >= 10
+-- Show score and name colums order numerically if score >= 10
+-- Query that shows the score and name colums ordered numerically if score >= 10
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
