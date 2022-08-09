1070b25bd96691fda43ca1fbf69bd93eace0f4ac
orahpsalm <psalmorahcle@gmail.com>
2022-08-09 21:25:32 +0100

    Update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
--- Create database with name htbn_0c_0
--- Query :creates hbn-0c_0 database in server if it is missing
-CREATE DATABASE IF NOT EXISTS `htbn_0c_0`;
+-- Script that creates the database htbn_0c_0 in MySQL server
+CREATE DATABASE IF NOT EXISTS htbn_0c_0;
