c3df76ec748f10ce247b128e5f589bd55526ab68
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-09-05 16:09:42 +0100

    Create 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+if (process.argv.length === 0)
+	console.log("No arguement")
+else if (process.argv.length === 1)
+	console.log("Argument found")
+else
+	console.log("Arguments found")
