714fe39d5147eed1c15c38e4855a6dc92b794824
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-09-06 19:51:07 +0100

    Create 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rectangle = '';
+    for (let i = 0; i < this.height; i++) {
+      rectangle = '';
+      for (let j = 0; j < this.width; j++) {
+        rectangle += 'X';
+      }
+      console.log(rectangle);
+    }
+  }
+};
