44d64f620cc8e5ee71da8bde64c304212ad08aa9
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-09-06 20:23:09 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    if (typeof c === 'undefined') {
-      c = 'X';
-    }
-    let square = '';
-    for (let i = 0; i < this.height; i++) {
-      square = '';
-      for (let j = 0; j < this.width; j++) {
-        square += c;
-      }
-      console.log(square);
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
