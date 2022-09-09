9afdfb7e5bfc2a9497c1cf7bbca77257a20aa8bb
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-09-09 20:43:26 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Module that creates a City class"""
-from model_state import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""Module which lists all states from the database"""
+import MySQLdb
+from sys import argv
 
-class City(Base):
-    """The class, City"""
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    res = cur.fetchall()
+    for r in res:
+        print(r)
+    cur.close()
+    db.close()
