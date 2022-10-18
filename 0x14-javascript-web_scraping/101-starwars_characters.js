c8d5eb4c0bac50f5e367ff22e2b834ecd3d00a80
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-10-18 21:02:54 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
+const axios = require('axios').default;
+function getName (url) {
+  return axios.get(url)
+    .then(response => {
+      console.log(response.data.name);
+    });
+}
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
+const displayData = async () => {
+  try {
+    const response = await axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2]);
+    for (const character of response.data.characters) {
+      await getName(character);
     }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+  } catch (err) {
+    console.log('Found error');
   }
-});
+};
+
+displayData();
