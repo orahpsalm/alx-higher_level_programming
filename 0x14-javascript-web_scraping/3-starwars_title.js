2c09b4e96ead740362b5d1b1bbcc21f7f7e6d1d4
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-10-18 20:49:01 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
-const axios = require('axios').default;
-axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
-  .then(function (response) {
-    console.log(response.data.title);
-  });
+
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
